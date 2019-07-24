a026c4f96fc0f1ff061e1d9ff4abc31c06529db8
llorentegerman <llorentegerman@gmail.com>
2019-07-24 14:27:35 +1200

    Updates

---
+++ precache-manifest.642efd9c905d8814ed17674d21b2b370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d234b9bb817fcc034b2dc54678cd2169",
+    "url": "/react-admin-dashboard/index.html"
+  },
+  {
+    "revision": "2b4f39e5f74733333554",
+    "url": "/react-admin-dashboard/static/css/main.0bc4455c.chunk.css"
+  },
+  {
+    "revision": "2a0e81728e43093f298d",
+    "url": "/react-admin-dashboard/static/js/2.ca5bed68.chunk.js"
+  },
+  {
+    "revision": "2b4f39e5f74733333554",
+    "url": "/react-admin-dashboard/static/js/main.49987f13.chunk.js"
+  },
+  {
+    "revision": "ea223c9ea25cce72d4ff",
+    "url": "/react-admin-dashboard/static/js/runtime~main.2bc0138f.js"
+  }
+]);
